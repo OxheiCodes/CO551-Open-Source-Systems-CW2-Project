c49c4b145238e4db546b847f9ac20901e10d3f48
Richard Ochei <162317835+OxheiCodes@users.noreply.github.com>
2024-05-06 16:26:44 +0100

    Commit

--- templates/partials/nav.php
+++
@@ -1,9 +0,0 @@
-<div id="main-nav" class="bg-dark text-white p-3 sticky-top">
-   <a href="modules.php" class="text-light">My Modules</a> |
-   <a href="assignmodule.php" class="text-light">Assign Module</a> |
-   <a href="details.php" class="text-light">My Details</a> |
-   <a href="students.php" class="text-light">Students</a> |
-   <a href="logout.php" class="text-light">Logout</a>
-</div>
-
-
